aa31203adb45e0adffea05c185f292ac92498bb3
rafnexcorp <109967465+rafnexcorp@users.noreply.github.com>
2025-12-15 02:27:04 +0800

    Update lookup.js

--- lookup.js
+++ lookup.js
@@ -142,7 +142,8 @@ Model: ${item.modelName}
 Serial Number: ${item.serialNumber}
 Asset Tag: ${item.assetTag}
             
-${jcRemovedLine}CMDB Updated: Y`;
+${jcRemovedLine}
+CMDB Updated: Y`;
 
         issuedOutputDiv.textContent = issuedTemplate;
         returnedOutputDiv.textContent = returnedTemplate;
